4b420430a1a396777685db8e2cbfaaaa3a7ffa97
Mikael Mamedov <mikaehlmamedv@gmail.com>
2021-10-12 08:54:03 +0300

    first commit

---
+++ src/main/java/com/mikaelmamedov/test/model/Socks.java
@@ -0,0 +1,55 @@
+package com.mikaelmamedov.test.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Socks {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private int cottonPart;
+    private String color;
+    private int quantity;
+
+    public Socks() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getCottonPart() {
+        return cottonPart;
+    }
+
+    public void setCottonPart(int cottonPart) {
+        this.cottonPart = cottonPart;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
